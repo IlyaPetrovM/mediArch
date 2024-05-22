46ffaa66b348ebe968660251fb882c622b0d1167
Ilya Petrov <ilya.petrov.m@gmail.com>
2024-05-22 10:14:24 +0300

    Отображение таблицы описи

--- public_html/js/files.js
+++ public_html/js/files.js
@@ -3,6 +3,7 @@
 ///
 const UPLOAD_PATH = 'uploads/'; // must en with '/'
 
+// Запрос к таблице
 const STANDARD_QUERY = `SELECT id, 
                         '>>>' as play,
                         'Просмотр' as view,
@@ -15,11 +16,16 @@ const STANDARD_QUERY = `SELECT id,
                         'Скачать' as download
                     FROM files `;   
 
-const icon_download = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M7.47 10.78a.75.75 0 001.06 0l3.75-3.75a.75.75 0 00-1.06-1.06L8.75 8.44V1.75a.75.75 0 00-1.5 0v6.69L4.78 5.97a.75.75 0 00-1.06 1.06l3.75 3.75zM3.75 13a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5z"></path></svg>';
+/**
+ * Картинки для кнопок
+ */
+const ICON_DOWNLOAD = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M7.47 10.78a.75.75 0 001.06 0l3.75-3.75a.75.75 0 00-1.06-1.06L8.75 8.44V1.75a.75.75 0 00-1.5 0v6.69L4.78 5.97a.75.75 0 00-1.06 1.06l3.75 3.75zM3.75 13a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5z"></path></svg>';
 
-const icon_play = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M9.5 15.584V8.416a.5.5 0 01.77-.42l5.576 3.583a.5.5 0 010 .842l-5.576 3.584a.5.5 0 01-.77-.42z"></path><path fill-rule="evenodd" d="M12 2.5a9.5 9.5 0 100 19 9.5 9.5 0 000-19zM1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12z"></path></svg>';
+const ICON_PLAY = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M9.5 15.584V8.416a.5.5 0 01.77-.42l5.576 3.583a.5.5 0 010 .842l-5.576 3.584a.5.5 0 01-.77-.42z"></path><path fill-rule="evenodd" d="M12 2.5a9.5 9.5 0 100 19 9.5 9.5 0 000-19zM1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12z"></path></svg>';
 
-
+/**
+ * Форматирование колонок таблицы
+ */
 const FORMAT_FILES_COLUMNS = [
     {
         field: 'id', 
@@ -94,7 +100,7 @@ const FORMAT_FILES_COLUMNS = [
     
     {
         field:'play',
-        formatter: () => {return icon_play },
+        formatter: () => {return ICON_PLAY },
         width:     20, 
         hozAlign:  "center",
         cellClick: function(e, cell){ playFile(e, cell.getRow().getData().name, true);} 
@@ -109,7 +115,7 @@ const FORMAT_FILES_COLUMNS = [
     {
         title: "Скачать",
         field: 'download',
-        formatter:function(){return icon_download;}, 
+        formatter:function(){return ICON_DOWNLOAD;}, 
         width:     20, 
         hozAlign:  "center",  
         cellClick: function(e, cell){ downloadFile(e, UPLOAD_PATH + cell.getRow().getData().name);} 
@@ -118,11 +124,13 @@ const FORMAT_FILES_COLUMNS = [
 
 
 /**
-*  downloadFile
+*  downloadFile - Открывает ссылку на определённый файл в новой вкладке
+*  eventOnClick - событие при нажатии кнопки загрузки файла
+*  path - полный путь к файлу, который нужно загружать
 */
-function downloadFile(event, path){
+function downloadFile(eventOnClick, path){
    window.open(path,"_blank");
-   event.stopPropagation();
+   eventOnClick.stopPropagation();
 }   
 
 
@@ -140,15 +148,14 @@ srch.addEventListener('keydown', function(e){
 function startSearch() {
     let where = ''
     if (!(srch.value === undefined || srch.value === '')){
-    where = `  WHERE (description like '%${srch.value}%' OR recognizedText like '%${srch.value}%' OR oldName like '%${srch.value}%' OR name like '%${srch.value}%' OR fileType like '%${srch.value}%' )`
+        where = `  WHERE (description like '%${srch.value}%' OR recognizedText like '%${srch.value}%' OR oldName like '%${srch.value}%' OR name like '%${srch.value}%' OR fileType like '%${srch.value}%' )`
     }
-    loadDataToDable(STANDARD_QUERY + where);
+    loadDataToTable(STANDARD_QUERY + where);
 }
 /**
  * MAIN CODE -  начало основного кода
  */
 
-console.log('creating table');
 var table = new Tabulator("#fileTable", {
     height:"100%",
     layout: "fitColumns",
@@ -160,8 +167,7 @@ var table = new Tabulator("#fileTable", {
     
 });
 table.on('tableBuilt', function(e){ 
-    console.log('ready!')
-    loadDataToDable(STANDARD_QUERY)
+    loadDataToTable(STANDARD_QUERY)
 });
 
 
@@ -169,7 +175,7 @@ table.on('tableBuilt', function(e){
  * @brief Запрос к БД. 
  * @return Обновляет таблицу на странице.
  */
-function loadDataToDable(q) {
+function loadDataToTable(q) {
     table.setData("api/sql/dataOnly", {
         'query': q,
         'inserts': ''
@@ -191,6 +197,7 @@ function getUrlExtention( url ) {
 
  /**
 * Запуск просмотра файла в окне предпросмотра
+* previewImg, previewVideo, peviewIframe - идентефикаторы элементов, где будут появляться предпросмотр
 */
 function playFile(event, name, useProxy=false){
    let path = getFilePath(name, useProxy);
---
+++ public_html/js/player.js
@@ -0,0 +1,51 @@
+// player.js
+
+const UPLOAD_PATH = 'uploads/'; // must en with '/'
+/**
+*
+*/
+function getFilePath(filename, useProxy=true){
+   return UPLOAD_PATH + filename;
+}
+/**
+* Определяет расширение файла
+*/
+function getUrlExtention( url ) {
+  return url.split(/[#?]/)[0].split('.').pop().trim().toLowerCase();
+}
+
+
+ /**
+* Запуск просмотра файла в окне предпросмотра
+* previewImg
+* previewImg, previewVideo, peviewIframe - идентефикаторы элементов, где будут появляться предпросмотр
+*/
+function playFile(event, name, useProxy=false){
+   let path = getFilePath(name, useProxy);
+   let ext = getUrlExtention(path);
+    
+   previewImg.hidden = previewVideo.hidden = previewIframe.hidden = true;
+   switch(ext){
+      case 'mkv':
+      case 'mov':
+      case 'wav':
+      case 'aac':
+      case 'mp3':
+      case 'mp4': 
+        previewVideo.src = path; 
+        previewVideo.hidden = false;
+        break;
+      case 'gif':
+      case 'bmp':
+      case 'svg':
+      case 'png':
+      case 'tif':
+      case 'jpg':
+        previewImg.src = path;
+        previewImg.hidden = false;
+        break;
+      default:
+        previewIframe.src = path;
+        previewIframe.hidden = false;
+   } 
+}
