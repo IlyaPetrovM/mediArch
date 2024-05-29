f3b507368ce33bbc8f4b8dcff2ece30436ea36e5
Ilya Petrov <ilya.petrov.m@gmail.com>
2024-05-29 19:12:00 +0300

    v0.0.22 Добавление типов файлов

--- create_database_structure.sql
+++ create_database_structure.sql
@@ -1,10 +1,3 @@
--- --------------------------------------------------------
--- Хост:                         127.0.0.1
--- Версия сервера:               10.5.10-MariaDB - mariadb.org binary distribution
--- Операционная система:         Win64
--- HeidiSQL Версия:              11.2.0.6213
--- --------------------------------------------------------
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
@@ -12,24 +5,21 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Дамп структуры для таблица test_file_uploader.files
 CREATE TABLE IF NOT EXISTS `files` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(512) COLLATE utf8mb4_unicode_ci DEFAULT '',
   `time_upload` datetime DEFAULT current_timestamp(),
   `oldName` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `filetype` char(4) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `fileExt` char(4) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `filetype` char(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `preview` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `description` text COLLATE utf8mb4_unicode_ci DEFAULT '',
   `recognizedText` text COLLATE utf8mb4_unicode_ci DEFAULT '',
   `recognitionStatus` char(50) COLLATE utf8mb4_unicode_ci DEFAULT 'Ещё не распознан',
   `hashsum` varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Экспортируемые данные не выделены.
-
--- Дамп структуры для таблица test_file_uploader.interfaces
 CREATE TABLE IF NOT EXISTS `interfaces` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tableName` char(50) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -37,11 +27,8 @@ CREATE TABLE IF NOT EXISTS `interfaces` (
   `editorHtml` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `viewHtml` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Экспортируемые данные не выделены.
-
--- Дамп структуры для таблица test_file_uploader.marks
 CREATE TABLE IF NOT EXISTS `marks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `start_time` time DEFAULT NULL,
@@ -56,9 +43,7 @@ CREATE TABLE IF NOT EXISTS `marks` (
   FULLTEXT KEY `tags` (`tags`),
   FULLTEXT KEY `decription_of_file` (`describtion`),
   CONSTRAINT `marks_to_files_constr` FOREIGN KEY (`file_id`) REFERENCES `files` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
-
--- Экспортируемые данные не выделены.
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
