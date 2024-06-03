37d9d612e510310b32e0c5200bc3c9f96725d5fc
Ilya Petrov <ilya.petrov.m@gmail.com>
2024-06-03 22:18:31 +0300

    Добавление и редактирование информантов

--- public_html/js/informants.js
+++ public_html/js/informants.js
@@ -1,10 +1,15 @@
 const FORMAT_INFORMANTS_COLUMNS = [
+{
+        field: 'id',
+        width:24
 
+    },
     //  last_name
     {
         field: 'last_name',
         title: 'Фамилия',
         editor: 'list',
+        width:128,
         editorParams: {
             autocomplete: "true",
             allowEmpty: true,
@@ -21,6 +26,7 @@ const FORMAT_INFORMANTS_COLUMNS = [
         field: 'first_name',
         title: 'Имя',
         editor: 'list',
+        width:128,
         editorParams: {
             autocomplete: "true",
             allowEmpty: true,
@@ -30,13 +36,14 @@ const FORMAT_INFORMANTS_COLUMNS = [
         },
         cellEdited: async (cell) => {
             let res = await sql(`UPDATE informants SET first_name='${cell.getValue()}' WHERE id=${cell.getRow().getData().id}`);
-        },
+        }
     },
     //  middle_name
     {
         field: 'middle_name',
         title: 'Отчество',
         editor: 'list',
+        width:128,
         editorParams: {
             autocomplete: "true",
             allowEmpty: true,
@@ -46,14 +53,15 @@ const FORMAT_INFORMANTS_COLUMNS = [
         },
         cellEdited: async (cell) => {
             let res = await sql(`UPDATE informants SET middle_name='${cell.getValue()}' WHERE id=${cell.getRow().getData().id}`);
-            if (res.errors) alert('Не удалось обновить поле')
+            if (res.errors) {alert('Не удалось обновить поле');}
         },
     },
      //  birthYear
     {
         field: 'birthYear',
-        title: 'Год рождения',
+        title: 'г/р',
         editor: 'input',
+        width:64,
         editorParams:{
             elementAttributes: {
                 type:'search',
@@ -65,45 +73,47 @@ const FORMAT_INFORMANTS_COLUMNS = [
         cellEdited: async (cell) => {
             let newVal = "'"+cell.getValue()+"'";
             console.log(newVal)
-            if (newVal === "''" || newVal == undefined || newVal.length < 3)
+            if (newVal === "''" || newVal == undefined || newVal.length < 3){
                 newVal = 'NULL';
-            console.log(newVal)
+            }
+            console.log(newVal);
             let res = await sql(`UPDATE informants SET birthYear=${newVal} WHERE id=${cell.getRow().getData().id}`);
             if (res.errors){
-                alert('Не удалось обновить поле. Вводите только 4 цифры года')
-                cell.setValue(cell.getOldValue())
+                alert('Не удалось обновить поле. Вводите только 4 цифры года');
+                cell.setValue(cell.getOldValue());
             }
-        },
+        }
     },
+
     //  birth
     {
         field: 'birth',
-        title: 'Дата рождения',
+        title: 'д/р',
         editor: 'date',
-        formatter:(cell) => {
-            console.log(cell.getValue())
-            if (cell.getValue() !== null )
-                return luxon.DateTime.fromISO(cell.getValue()).toFormat('dd.MM.yyyy')
-            else return null
-        },
+        width:100,
         editorParams:{
-            format:"dd.MM.yyyy", // the format of the date value stored in the cell
-//            elementAttributes: {
-//                type:'date'
-//            }
+            format:"yyyy-MM-dd"
+        },
+        formatter:function(cell) {
+            console.log(cell.getValue())
+            if (cell.getValue() == null ) return null;
+            return luxon.DateTime.fromISO(cell.getValue()).toFormat('dd.MM.yyyy');
+//            if (cell.getValue() != null )
+//                return luxon.DateTime.fromFormat('yyyy.MM.dd hh:mm:ss',cell.getValue()).toFormat('dd.MM.yyyy hh:mm:ss');
+//            else return null;
         },
         cellEdited: async (cell) => {
-
-            let newVal = "'" + luxon.DateTime.fromFormat(cell.getValue(), "dd.MM.yyyy").toFormat("yyyy-MM-dd") + "'"
+            let newVal = "'" + luxon.DateTime.fromFormat(cell.getValue(), "yyyy-MM-dd").toFormat("yyyy-MM-dd") + "'"
             console.log(newVal)
             if (newVal === "''" || newVal == "'Invalid DateTime'" || newVal.length < 3)
                 newVal = 'NULL';
             console.log(newVal)
             let res = await sql(`UPDATE informants SET birth=${newVal} WHERE id=${cell.getRow().getData().id}`);
             if (res.errors){
-                alert('Не удалось обновить поле')
-                cell.setValue(cell.getOldValue())
+                alert('Не удалось обновить поле');
+                cell.setValue(cell.getOldValue());
             }
+            cell.getRow().getCell('birthYear').setValue(luxon.DateTime.fromFormat(cell.getValue(), "yyyy-MM-dd").toFormat("yyyy"));
         }
     },
    //  comments
@@ -111,53 +121,73 @@ const FORMAT_INFORMANTS_COLUMNS = [
         field: 'comments',
         title: 'Заметки',
         editor: 'input',
-//        editorParams:{
-//            elementAttributes:{
-//                selectContents:true,
-//                verticalNavigation:"editor", //navigate cursor around text area without leaving the cell
-//                shiftEnterSubmit:true, //submit cell value on shift enter
-//            }
-//        },
+        // BUG!!! При использовании поля textarea почему то не удаётся ввести больше одной буквы
+        width:200,
         cellEdited: async (cell) => {
             let res = await sql(`UPDATE informants SET comments='${cell.getValue()}' WHERE id=${cell.getRow().getData().id}`);
-            if (res.errors) alert('Не удалось обновить поле')
-        },
+            if (res.errors) alert('Не удалось обновить поле');
+        }
     },
     //  contacts
     {
         field: 'contacts',
         title: 'Контакты',
         editor: 'input',
-//        editorParams:{
-//            elementAttributes:{
-//                selectContents:true,
-//                verticalNavigation:"editor", //navigate cursor around text area without leaving the cell
-//                shiftEnterSubmit:true, //submit cell value on shift enter
-//            }
-//        },
+        width:200,
         cellEdited: async (cell) => {
             let res = await sql(`UPDATE informants SET contacts='${cell.getValue()}' WHERE id=${cell.getRow().getData().id}`);
-            if (res.errors) alert('Не удалось обновить поле')
-        },
+            if (res.errors) alert('Не удалось обновить поле');
+        }
     },
-    //  keywords  BUG!!! При использовании поля textarea почему то не удаётся ввести больше одной буквы
+    //  keywords
     {
         field: 'keywords',
         title: 'Ключевые слова',
-        editor: 'input',
-//        editorParams:{
-//            elementAttributes:{
-//                selectContents:true,
-//                verticalNavigation:"editor", //navigate cursor around text area without leaving the cell
-//                shiftEnterSubmit:true, //submit cell value on shift enter
-//            }
-//        },
+        editor: 'list',
+        editorParams:{autocomplete:"true", allowEmpty:true,listOnEmpty:true, valuesLookup:true, freetext:true},
         cellEdited: async (cell) => {
             let res = await sql(`UPDATE informants SET contacts='${cell.getValue()}' WHERE id=${cell.getRow().getData().id}`);
-            if (res.errors) alert('Не удалось обновить поле')
-        },
+            if (res.errors) alert('Не удалось обновить поле');
+        }
     },
-    //  hide
+
+    //  reporter
+    {
+        field: 'reporter',
+        title: 'Знакомый собиратель',
+        editor: 'input',
+        cellEdited: async (cell) => {
+            let res = await sql(`UPDATE informants SET reporter='${cell.getValue()}' WHERE id=${cell.getRow().getData().id}`);
+            if (res.errors) alert('Не удалось обновить поле');
+        }
+    },
+    //  user_created
+    {
+        field: 'user_created',
+        title: 'Кто записал'
+    },
+    //  date_updated -- BUG!! Перестал отображаться
+    {
+        field: 'date_updated',
+        title: 'Время обновления',
+        formatter:function(cell) {
+            console.log(cell.getValue())
+            if (cell.getValue() !== null )
+                return luxon.DateTime.fromISO(cell.getValue()).toFormat('dd.MM.yyyy hh:mm:ss');
+            else return null;
+        }
+    },
+//      date_created
+    {
+        field: 'date_created',
+        title: 'Время создания',
+        formatter:(cell) => {
+            if (cell.getValue() !== null )
+                return luxon.DateTime.fromISO(cell.getValue()).toFormat('dd.MM.yyyy hh:mm:ss')
+            else return null
+        }
+    },
+        //  hide
     {
         field: 'hide',
         title: 'Скрыть',
@@ -169,52 +199,16 @@ const FORMAT_INFORMANTS_COLUMNS = [
             if(!ans)return;
             let edit_result = await sql(
                 `UPDATE informants SET hide = 1 WHERE id = ${cell.getRow().getData().id}`)
-            if (edit_result.errors) {alert('Ошибка при сохранении описания метки в БД')}
-            cell.getRow().delete()
+            if (edit_result.errors) {
+                alert('Ошибка при сохранении описания метки в БД');
+            }
+            cell.getRow().delete();
         }
-
-    },
-    //  reporter
-    {
-        field: 'reporter',
-        title: 'Знакомый собиратель',
-        editor: 'input',
-        cellEdited: async (cell) => {
-            let res = await sql(`UPDATE informants SET reporter='${cell.getValue()}' WHERE id=${cell.getRow().getData().id}`);
-            if (res.errors) alert('Не удалось обновить поле')
-        },
-    },
-    //  user_created
-    {
-        field: 'user_created',
-        title: 'Кто записал',
-    },
-    //  date_updated
-    {
-        field: 'date_updated',
-        title: 'Время обновления',
-        formatter:(cell) => {
-            console.log(cell.getValue())
-            if (cell.getValue() !== null )
-                return luxon.DateTime.fromISO(cell.getValue()).toFormat('dd.MM.yyyy hh:mm:ss')
-            else return null
-        }
-    },
-    //  date_created
-    {
-        field: 'date_created',
-        title: 'Время создания',
-        formatter:(cell) => {
-            console.log(cell.getValue())
-            if (cell.getValue() !== null )
-                return luxon.DateTime.fromISO(cell.getValue()).toFormat('dd.MM.yyyy hh:mm:ss')
-            else return null
-        }
-    },
+    }
 ];
 var USER = ''
 
-const QUERY_INFORMANTS = `SELECT * from informants WHERE hide is null ORDER BY last_name ASC`;
+const QUERY_INFORMANTS = `SELECT * FROM informants`;
 
 async function getUsername(){
     let user = ''
@@ -229,13 +223,15 @@ async function getUsername(){
     }
 }
 
-async function runInformants(){
-    let table = new Tabulator('#informantsTable',{
+    var table = new Tabulator('#informantsTable',{
         height:'100%',
         placeholder: 'Введите фразу в поиске',
+        ajaxContentType: "json",
+        layout: "fitColumns",
         autoColumns: true,
         autoColumnsDefinitions: FORMAT_INFORMANTS_COLUMNS
     });
+async function runInformants(){
 
     table.on('tableBuilt', function(e){
         table.setData("api/sql/dataOnly", {
@@ -246,6 +242,23 @@ async function runInformants(){
     USER = (await getUsername()).data
     console.log(USER)
 
+    document.getElementById('buttonAddInformant').addEventListener('click',function(){
+        addInformant(table, USER)
+    })
 }
 runInformants()
 
+async function addInformant(tab, user){
+    let res = await sql( `INSERT INTO informants (user_created) VALUES ('${user}')`)
+    if(res.errors){
+        alert("Ошибка при добавлении Иинформанта")
+        return;
+    }
+    let row = tab.addRow({
+        id: res.data.insertId,
+        user_created: user,
+        birth:null
+    });
+    tab.deselectRow();
+    tab.selectRow(res.data.insertId);
+}
