f8aa302973cd98cd45e5a40041c5bea4d8a6369f
Ilya Petrov <ilya.petrov.m@gmail.com>
2026-06-29 00:07:27 +0300

    update config

--- app/sample-config.js
+++ app/sample-config.js
@@ -2,11 +2,14 @@ const config = {
     YANDEX_API: 'xxxxxxx-xxxx-xxx-xxx-xxxxxxxxxxx',
     PORT: 3000,
     UPLOAD_PATH:__dirname + '/public_html/uploads/',
+    CHUNKS_DIR:__dirname + '/public_html/upload_parts/',
+    MASTER_WISPER_URL: process.env.MASTER_WISPER_URL || 'http://master-wisper:8000',
+    FILE_STORAGE_URL: process.env.FILE_STORAGE_URL || 'http://file-storage-service:3001',
     DB:{
-        host: "localhost",
-        database: "test_file_uploader",
-        user: "YOUR_DATABASE_USER",
-        password: 'YOUR_DATABASE_PASSWORD'
+        host: process.env.DB_HOST || 'localhost',
+        database: "mediarch",
+        user: "mediarch_user",
+        password: 'mediarch_password'
     }
 }
 module.exports = config
